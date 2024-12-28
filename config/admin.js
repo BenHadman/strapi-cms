488ede369df7a684df228efc22a8f4e1f9d181e8
BenHadman <benhadman@gmail.com>
2024-12-28 15:28:30 +0000

    Prepare Strapi project for Railway deployment

--- config/admin.js
+++ config/admin.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
     secret: env('ADMIN_JWT_SECRET'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'D3W4R+oEQP4abj9p0vQvGA=='),
   },
   transfer: {
     token: {
